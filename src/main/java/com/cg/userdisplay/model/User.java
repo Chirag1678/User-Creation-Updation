b2af5b0974c6731b87475aef4cd63c67aad891ee
Chirag <chiraggarg1708@gmail.com>
2025-04-29 11:24:23 +0530

    [Chirag] ADD: Changed method from session to sending hidden values

--- src/main/java/com/cg/userdisplay/model/User.java
+++ src/main/java/com/cg/userdisplay/model/User.java
@@ -1,8 +1,13 @@
 package com.cg.userdisplay.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private String firstName;
     private String lastName;
